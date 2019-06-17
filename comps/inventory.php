94bf1e51f9546a250a3e97ebb8936b98b4facef0
ashanowski <ashanowski@cock.li>
2019-06-17 17:42:02 +0200

    koszyk dziala prowizorycznie

--- comps/inventory.php
+++ comps/inventory.php
@@ -26,34 +26,86 @@
     }
     $connection->close();
 
-    
+    /*
+     * ------- BASKET ADDING ITEMS
+     */
 
-    function printShop($id, $name, $weight, $price, $image)
+    if (!isset($_SESSION['basket'])) {
+        $_SESSION['basket'] = [];
+    }
+
+    if (isset($_POST['quantity']) && !empty($_POST['quantity'])) {
+
+        $order = (object) [
+                'id' => $_POST['id'],
+                'quantity' => $_POST['quantity']
+        ];
+
+        array_push($_SESSION['basket'], $order);
+
+    }
+    function printShop($name, $weight, $price, $image, $id)
     {
-        echo 
-            '<div class="item">
-                <form method="post" action="index.php?action=add&id='.$id.'">
-                <div class="item-name">
-                    <h2>'.$name.'</h2>
+        echo "<div class='item'>
+                <div class='item-name'>
+                    <h2>$name</h2>
                 </div>
     
-                <div class="item-details">
-                    <div class="item-image">
-                        <img src="assets/inv_images/'.$image.'" alt="">
+                <div class='item-details'>
+                    <div class='item-image'>
+                        <img src='assets/inv_images/$image' alt=''>
                     </div>
-                    <div class="item-desc">
-                        <p class="item-weight">'.$weight.'kg</p>
-                        <h4 class="item-price">'.$price.'.00zł</h4>
-                        <div class="cart-action">
-                            <input type="text" class="product-quantity" name="quantity" value="1" size="2" />
-                            <input type="submit" value="Dodaj do koszyka" class="btnAddAction" />
-                        </div>
+                    <div class='item-desc'>
+                        <p class='item-weight'>$weight kg</p>
+                        <h4 class='item-price'>$price.00zł</h4>
+                        <form method='post'>
+                            <input type='hidden' name='id' value='$id'>
+                            <label for='quantity'>Ilość zamówienia:</label>
+                            <input type='number' name='quantity'> 
+                            <button class='item-add'>Dodaj do koszyka</button>
+                        </form>               
                     </div>
                 </div>
-            </div>';
+            </div>";
     }
+
+    //foreach($inventory as $item)
+    //{
+    //    printShop($item["name"], $item["weight"], $item["price"], $item["image_ref"]);
+    //}
+    //printShop();
+    //printShop();
     ?>
+<!--
 <div class="inventory">
+    <div class="item">
+
+        <div class="item-name">
+            <h2>Royal Canin Size</h2>
+        </div>
+
+        <div class="item-details">
+            <div class="item-image">
+                <img src="assets/example_image.jpg" alt="">
+            </div>
+            <div class="item-desc">
+                <p class="item-weight">18kg</p>
+                <h4 class="item-price">180.00zł</h4>
+                <button class="item-add">Dodaj do koszyka</button>
+            </div>
+        </div>
+    </div>
+</div>
+-->
+<div class="inventory">
+    <section class="basket">
+        <h1>Koszyk:</h1>
+        <br>
+        <?php foreach ($_SESSION['basket'] as $item => $value) {
+            echo 'ID: '.$value->id.', ilość: '.$value->quantity,'<br>';
+        }
+        ?>
+    </section>
     <div class="sort">
         <h2>Wyświetl karmy dla:</h2>
             <form method = "post">
@@ -69,119 +121,30 @@
                 <input type="submit" value="Wyświetl">
             </form>
     </div>
-
-<!-- // $all_species = array('DOG','CAT');
-// $selected_species = array();
-// foreach($all_species as $animal)
-// {
-//     if(isset($_POST[$animal]))
-//     {
-//         array_push($selected_species, $animal);
-//     }
-// }
-
-// foreach($inventory as $item)
-//     {
-//         if(isset($_POST["all"]) or count($selected_species)==0)
-//         {
-//             printShop($item["id"], $item["name"], $item["weight"], $item["price"], $item["image_ref"]);
-//         }
-//         elseif(in_array($item["species"], $selected_species))
-//         {
-//             printShop($item["id"], $item["name"], $item["weight"], $item["price"], $item["image_ref"]);
-//         }
-        
-//     } -->
-
 <?php
-// session_start();
-require_once (dirname(__DIR__).'/php/dbcontroller.php');
-$db_handle = new DBController();
-if (!empty($_GET["action"])) {
-    switch ($_GET["action"]) {
-        case "add":
-            if (! empty($_POST["quantity"])) {
-                $productByCode = $db_handle->runQuery("SELECT * FROM inventory WHERE id='" .$_GET["id"]. "'");
-                $itemArray = array(
-                    $productByCode[0]["id"] => array(
-                        'name' => $productByCode[0]["name"],
-                        'id' => $productByCode[0]["id"],
-                        'quantity' => $_POST[0]["quantity"],
-                        'price' => $productByCode[0]["price"],
-                        'image' => $productByCode[0]["image_ref"]
-                    )
-                );
-
-                if (! empty($_SESSION["cart_item"])) {
-                    if (in_array($productByCode[0]["id"], array_keys($_SESSION["cart_item"]))) {
-                        foreach ($_SESSION["cart_item"] as $k => $v) {
-                            if ($productByCode[0]["id"] == $k) {
-                                if (empty($_SESSION["cart_item"][$k]["quantity"])) {
-                                    $_SESSION["cart_item"][$k]["quantity"] = 0;
-                                }
-                                $_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
-                            }
-                        }
-                    } else {
-                        $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"], $itemArray);
-                    }
-                } else {
-                    $_SESSION["cart_item"] = $itemArray;
-                }
-            }
-            break;
-
-        case "remove":
-            if (!empty($_SESSION["cart_item"])) {
-                foreach($_SESSION["cart_item"] as $k => $v) {
-                    if ($_GET["id"] == $k)
-                        unset($_SESSION["cart_item"][$k]);
-                    if (empty($_SESSION["cart_item"]))
-                        unset($_SESSION["cart_item"]);
-                }
-            }
-            break;
-        
-        case "empty":
-            unset($_SESSION["cart_item"]);
-            break;
+$all_species = array('DOG','CAT');
+$selected_species = array();
+foreach($all_species as $animal)
+{
+    if(isset($_POST[$animal]))
+    {
+        array_push($selected_species, $animal);
     }
 }
-?>
 
-<?php
-$product_array = $db_handle->runQuery("SELECT * FROM inventory ORDER BY id ASC");
-if (!empty($product_array)) {
-    foreach ($product_array as $key => $value) {
-        ?>
-        <div class="item">
-            <form method="post" 
-                action="inventory.php?action=add&id=<?php echo $product_array[$key]["id"];?>">
-            <div class="item-name">
-                <h1>ID: <?php echo $product_array[$key]["id"] ?></h1>
-                <h2><?php echo $product_array[$key]["name"];?></h2>
-            </div>
-
-            <div class="item-details">
-                <div class="item-image">
-                    <img src="assets/inv_images/<?php echo $product_array[$key]["image_ref"];?>" alt="">
-                </div>
-                <div class="item-desc">
-                    <p class="item-weight"><?php echo $product_array[$key]["weight"];?> kg</p>
-                    <h4 class="item-price"><?php echo $product_array[$key]["price"];?>.00 zł</h4>
-                    <div class="cart-action">
-                        <input type="text" class="product-quantity" name="quantity" value="1" size="2" />
-                        <input type="submit" value="Dodaj do koszyka" class="btnAddAction" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php
+foreach($inventory as $item)
+    {
+        if(isset($_POST["all"]) or count($selected_species)==0)
+        {
+            printShop($item["name"], $item["weight"], $item["price"], $item["image_ref"], $item['id']);
+        }
+        elseif(in_array($item["species"], $selected_species))
+        {
+            printShop($item["name"], $item["weight"], $item["price"], $item["image_ref"], $item['id']);
+        }
+        
     }
-}
 ?>
-
-
 </div>
 
 </html>
